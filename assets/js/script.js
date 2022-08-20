a5a3733502fa38281b5abd9f48f6b65db580fc9e
gcgavinoc <gcgavinoc@gmail.com>
2022-08-20 13:59:39 +0000

    Add and style help button

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+/**
+ * Function for putting selection choice into player selection area
+ * on selection button click
+ */
+
+/**
+ * Function for filling computer selection area on shoot button click
+ */
+
+/**
+ * Function for checking if player selection area beats computer selection area
+ * on shoot button click
+ */
+
+/**
+ * Function for incrementing player score
+ */
+
+/**
+ * Function for incrementing computer score and reducing lives number
+ */
+
+/**
+ * Function for lose screen when lives equal 0
+ */
+
+/**
+ * Function for win screen when computer score reaches 18
+ */
