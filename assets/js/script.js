9a074400c97198edc5a25b4c00f2e5145ad69a45
gcgavinoc <gcgavinoc@gmail.com>
2022-08-20 15:03:33 +0000

    Add javascript functions for filling player choice area with selection

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,33 @@
  * Function for putting selection choice into player selection area
  * on selection button click
  */
+let rock = document.getElementById('rock');
+let paper = document.getElementById('paper');
+let scissors = document.getElementById('scissors');
+let lizard = document.getElementById('lizard');
+let spock = document.getElementById('spock');
+let playerChoice = document.getElementById('my-choice');
+rock.addEventListener("click", fillChoiceRock);
+paper.addEventListener("click", fillChoicePaper);
+scissors.addEventListener("click", fillChoiceScissors);
+lizard.addEventListener("click", fillChoiceLizard);
+spock.addEventListener("click", fillChoiceSpock);
+
+function fillChoiceRock(){
+    playerChoice.innerHTML = rock.innerHTML;
+}
+function fillChoicePaper(){
+    playerChoice.innerHTML = paper.innerHTML;
+}
+function fillChoiceScissors(){
+    playerChoice.innerHTML = scissors.innerHTML;
+}
+function fillChoiceLizard(){
+    playerChoice.innerHTML = lizard.innerHTML;
+}
+function fillChoiceSpock(){
+    playerChoice.innerHTML = spock.innerHTML;
+}
 
 /**
  * Function for filling computer selection area on shoot button click
